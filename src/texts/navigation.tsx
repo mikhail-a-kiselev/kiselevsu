cd2b153e2fc58204e08863d3cd04554a0a0761de
Mikhail Kiselev <mikhail.kiselev@simbirit.ru>
2022-06-22 17:45:37 +0400

    navigation texts

---
+++ src/texts/navigation.tsx
@@ -0,0 +1,5 @@
+export const MIKHAIL_KISELEV = "Михаил Киселев";
+export const MIKHAIL_KISELEVS_PERSONAL_SITE = "Персональный сайт Михаила Киселева"
+export const ABOUT = "Обо мне";
+export const PORTFOLIO = "Портфолио";
+export const CONTACT = "Контактная информация";
