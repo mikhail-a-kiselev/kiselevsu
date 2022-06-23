e14b2a8e11dec74169b0b40618b1accd7847e988
Mikhail Kiselev <mikhail.kiselev@simbirit.ru>
2022-06-23 11:04:52 +0400

    static html export, portfolio page start

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
-
+import { Card } from 'react-bootstrap'
 function Portfolio () {
-  return <div>My portfolio</div>
+  return <div>
+    <Card>
+      <Card.Body>
+        <Card.Title>ЖК.онлайн виджет</Card.Title>
+        <Card.Text>
+          Здравствуйте, меня зовут Михаил Киселев, я frontend-разработчик.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card>
+      <Card.Body>
+        <Card.Title>ЖК.онлайн CRM для УК</Card.Title>
+        <Card.Text>
+          Здравствуйте, меня зовут Михаил Киселев, я frontend-разработчик.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card>
+      <Card.Body>
+        <Card.Title>Дети и наука</Card.Title>
+        <Card.Text>
+          Здравствуйте, меня зовут Михаил Киселев, я frontend-разработчик.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card>
+      <Card.Body>
+        <Card.Title>Мобильные игры</Card.Title>
+        <Card.Text>
+          Здравствуйте, меня зовут Михаил Киселев, я frontend-разработчик.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </div>
 }
 
 export default Portfolio
