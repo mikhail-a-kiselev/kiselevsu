199481958e378f30fad61632709c97d7efdbe8b9
Mikhail Kiselev <mikhail.kiselev@simbirit.ru>
2022-07-05 22:40:07 +0400

    social networks icons

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
+import github from '../assets/img/github.png'
+import tenchat from '../assets/img/tenchat.png'
+import hh from '../assets/img/hh.png'
+import vk from '../assets/img/vk.svg'
+import ok from '../assets/img/ok.svg'
 
 function Contact () {
   return <div>
@@ -36,12 +41,46 @@ function Contact () {
             content=""
         />
     </Head>
-    <p><a href="https://github.com/mikhail-a-kiselev">Github</a></p>
-    <p><a href="https://tenchat.ru/MikhailKiselev">TenChat</a></p>
-    <p><a href="https://vk.com/mikhail_kiselev">VK</a></p>
-    <p><a href="https://ok.ru/mikhailkiselev">Odnoklassniki</a></p>
-    <p><a href="https://ulyanovsk.hh.ru/resume/f3a055e4ff064e22de0039ed1f773952703973">HH</a></p>
+    <a className="contact-link contact-link-github" title="GitHub" href="https://github.com/mikhail-a-kiselev">Github</a>
+    <a className="contact-link contact-link-tenchat" title="TenChat" href="https://tenchat.ru/MikhailKiselev">TenChat</a>
+    <a className="contact-link contact-link-hh" title="HeadHunter" href="https://ulyanovsk.hh.ru/resume/f3a055e4ff064e22de0039ed1f773952703973">HH</a>
+    <a className="contact-link contact-link-vk" title="VK" href="https://vk.com/mikhail_kiselev">VK</a>
+    <a className="contact-link contact-link-ok" title="Odnoklassniki" href="https://ok.ru/mikhailkiselev">Odnoklassniki</a>
     <p><b>E-mail:</b> <a href="mailto:kiselev@simbirsk.ru">kiselev@simbirsk.ru</a></p>
+    <style jsx>{`
+      .contact-link {
+        font-size:0px;
+        width:96px;
+        height:64px;
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: contain;
+        display: inline-block;
+        opacity:0.6;
+        transition: 0.5s;
+        transition-property: opacity;
+        &:hover{
+          opacity:1;
+        }
+        &.contact-link{
+          &-github{
+            background-image:url(${github.src})
+          }
+          &-tenchat{
+            background-image:url(${tenchat.src})
+          }
+          &-hh{
+            background-image:url(${hh.src});
+          }
+          &-vk{
+            background-image:url(${vk.src})
+          }
+          &-ok{
+            background-image:url(${ok.src})
+          }
+        }
+      }
+    `}</style>
   </div>
 }
 
